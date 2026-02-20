a468d19d002e147bbe82e9a2dad66d62f1160995
frostfrazer <jmuranga489@gmail.com>
2026-02-20 16:39:13 +0300

    Remove debug files

--- app/test/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage({ params }: { params: { id: string } }) {
-  return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Dynamic Route Works!</h1>
-      <p className="text-xl mt-4">ID from URL: {params.id}</p>
-    </div>
-  );
-}
